9081089806d2c97fcae14ee84f1ef177261ce50f
Amit7366 <omit9090@gmail.com>
2025-05-08 17:50:04 +0600

    till about

---
+++ js/script.js
@@ -0,0 +1,68 @@
+$(".count").each(function () {
+    $(this)
+        .prop("Counter", 0)
+        .animate(
+            {
+                Counter: $(this).text(),
+            },
+            {
+                duration: 2000,
+                easing: "swing",
+                step: function (now) {
+                    now = Number(Math.ceil(now)).toLocaleString('en');
+                    $(this).text(now);
+                },
+            }
+        );
+});
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+AOS.init();
+
+
+// let darkTheme = document.getElementById('darkTheme');
+
+// darkTheme.oneclick = function(){
+//     document.body.classList.toggle("dark-theme");
+// }
+$(function(){
+	$(".typed").typed({
+		strings: ["Developers.", "Designers.", "People."],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 30,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
