81b6a245f222a78bc7ac2d599a4f1cac1460143a
Amit7366 <omit9090@gmail.com>
2025-05-19 17:37:56 +0600

    updated banner pprtion

--- js/script.js
+++ js/script.js
@@ -32,39 +32,39 @@ AOS.init();
 //     document.body.classList.toggle("dark-theme");
 // }
 $(function () {
-	$(".typed").typed({
-		strings: ["Developers.", "Designers.", "People."],
-		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-		stringsElement: null,
-		// typing speed
-		typeSpeed: 30,
-		// time before typing starts
-		startDelay: 1200,
-		// backspacing speed
-		backSpeed: 20,
-		// time before backspacing
-		backDelay: 500,
-		// loop
-		loop: true,
-		// false = infinite
-		loopCount: 5,
-		// show cursor
-		showCursor: false,
-		// character for cursor
-		cursorChar: "|",
-		// attribute to type (null == text)
-		attr: null,
-		// either html or text
-		contentType: 'html',
-		// call when done callback function
-		callback: function () { },
-		// starting callback function before each string
-		preStringTyped: function () { },
-		//callback for every typed string
-		onStringTyped: function () { },
-		// callback for reset
-		resetCallback: function () { }
-	});
+	// $(".typed").typed({
+	// 	strings: ["Developers.", "Designers.", "People."],
+	// 	// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+	// 	stringsElement: null,
+	// 	// typing speed
+	// 	typeSpeed: 30,
+	// 	// time before typing starts
+	// 	startDelay: 1200,
+	// 	// backspacing speed
+	// 	backSpeed: 20,
+	// 	// time before backspacing
+	// 	backDelay: 500,
+	// 	// loop
+	// 	loop: true,
+	// 	// false = infinite
+	// 	loopCount: 5,
+	// 	// show cursor
+	// 	showCursor: false,
+	// 	// character for cursor
+	// 	cursorChar: "|",
+	// 	// attribute to type (null == text)
+	// 	attr: null,
+	// 	// either html or text
+	// 	contentType: 'html',
+	// 	// call when done callback function
+	// 	callback: function () { },
+	// 	// starting callback function before each string
+	// 	preStringTyped: function () { },
+	// 	//callback for every typed string
+	// 	onStringTyped: function () { },
+	// 	// callback for reset
+	// 	resetCallback: function () { }
+	// });
 
  $('#contact-form').on('submit', function (e) {
       e.preventDefault();
@@ -96,3 +96,30 @@ $(function () {
     });
 	
 });
+const words = [
+  "<i class='fas fa-hand-peace primary-color'></i> Hi,",
+  " I'm Sabbir",
+  "<i class='fas fa-code primary-color'></i> Full Stack Developer",
+  "<i class='fab fa-react primary-color'></i> React & Node.js",
+  "<i class='fas fa-server primary-color'></i> Express & TypeScript",
+  "<i class='fas fa-database primary-color'></i> MongoDB",
+  "<i class='fas fa-project-diagram primary-color'></i> GraphQL"
+];
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const instance = new TypeIt("#name", {
+      speed: 100,
+      breakLines: false,
+      loop: true,
+      deleteSpeed: 50,
+      nextStringDelay: 1000,
+      waitUntilVisible: true,
+    });
+
+    words.forEach((word) => {
+      instance.type(word).pause(1000).delete();
+    });
+
+    instance.go();
+  });
